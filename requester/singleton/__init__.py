2965517591205ca94114d6097639d190519c1123
dsantl <dsantl.ck@gmail.com>
2013-10-13 12:23:17 +0200

    Singleton u common, sys.path se koristi

--- requester/singleton/__init__.py
+++
@@ -1,6 +0,0 @@
-class RequestSingleton(object):
-    _instance = None
-    def __new__(cls):
-        if not cls._instance:
-            cls._instance = super(RequestSingleton, cls).__new__(cls)
-        return cls._instance
