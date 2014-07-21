43546d4179eb36582c75de4d814835038e52d041
dsantl <dsantl.ck@gmail.com>
2014-07-21 22:36:39 +0200

    pep8

--- joiner.py
+++ joiner.py
@@ -4,82 +4,82 @@ import sys
 import xml.etree.ElementTree as ET
 
 
-
 def generate_suggestion_list(error):
-	ret = list()
-	words = error.find("suggestions")
-	if words is None:
-		return ret
-	
-	for word in words:
-		ret.append(word.text)
+    ret = list()
+    words = error.find("suggestions")
+    if words is None:
+        return ret
 
-	return ret
+    for word in words:
+        ret.append(word.text)
+
+    return ret
+
 
 def return_solution(offset_list, solution_text):
-	
-	
-	error_words = list()
 
-	for item in offset_list:
-		errors, offset = item
-		for error in errors:
-			real_position = -1
-			real_lenght = -1
-			position = error.find("position")
-			lenght = error.find("length")
-			if position is not None:
-				real_position = int(position.text) + offset
-			if lenght is not None:
-				real_lenght = int(lenght.text)
-			
-			if real_position != -1 and real_lenght != -1:
-				word = solution_text[real_position:real_position+real_lenght]
-				suggestion_list = generate_suggestion_list(error) 
-				error_words.append((word, suggestion_list))
-	
-	start_find = 0
-	for errors in error_words:
-		word, suggestion_list = errors
-		print(word)
-		print(suggestion_list)
-	
+    error_words = list()
+
+    for item in offset_list:
+        errors, offset = item
+        for error in errors:
+            real_position = -1
+            real_lenght = -1
+            position = error.find("position")
+            lenght = error.find("length")
+            if position is not None:
+                real_position = int(position.text) + offset
+            if lenght is not None:
+                real_lenght = int(lenght.text)
+
+            if real_position != -1 and real_lenght != -1:
+                word = solution_text[real_position:real_position+real_lenght]
+                suggestion_list = generate_suggestion_list(error)
+                error_words.append((word, suggestion_list))
+
+    start_find = 0
+    for errors in error_words:
+        word, suggestion_list = errors
+        print(word)
+        print(suggestion_list)
+
+
 def get_all_errors(root):
 
-	ret_list = list()
+    ret_list = list()
 
-	for error in root.findall('error'):
-		ret_list.append(error)
+    for error in root.findall('error'):
+        ret_list.append(error)
 
-	for child in root:
-		ret_list += get_all_errors(child)
+    for child in root:
+        ret_list += get_all_errors(child)
 
-	return ret_list
+    return ret_list
+
 
 def main():
+    splitWord = "!__ENDOFTEXT__!"
+    endRQS = "!__ENDRQS__!"
 
-	splitWord = "!__ENDOFTEXT__!"
-	endRQS = "!__ENDRQS__!"
+    allText = "".join(sys.stdin.readlines())
+    offset = 0
+    offset_list = list()  # list of tuple(error_list, offset)
+    solution_text = ""  # allText without escape words
 
-	allText = "".join(sys.stdin.readlines())
-	offset = 0;
-	offset_list = list() #list of tuple(error_list, offset)
-	solution_text = "" #allText without escape words
+    rqs_list = allText.split(endRQS.rstrip())
 
-	rqs_list = allText.split(endRQS.rstrip())
+    for rqs in rqs_list:
 
-	for rqs in rqs_list:
-		
-		if rqs.find(splitWord) == -1:
-			continue
-		
-		text, errors = rqs.split(splitWord)
-		root = ET.fromstring(errors)
-		offset_list.append( (get_all_errors(root), offset) ) #append tupple
-		offset += len(text)
-		solution_text += text;
+        if rqs.find(splitWord) == -1:
+            continue
 
-	return_solution(offset_list, solution_text)
+        text, errors = rqs.split(splitWord)
+        root = ET.fromstring(errors)
+        offset_list.append((get_all_errors(root), offset))  # append tupple
+        offset += len(text)
+        solution_text += text
+
+    return_solution(offset_list, solution_text)
 
 if __name__ == "__main__":
-	main()
+    main()
--- requester.py
+++ requester.py
@@ -7,54 +7,54 @@ import threading
 import queue
 
 
-
 def send_request(text, answer_queue, thread_queue, id):
-	rqs = {"textarea":text}
-	r = requests.post("http://hacheck.tel.fer.hr/xml.pl", data = rqs)
-	answer_queue.put((id, (text, r.text)))
-	thread_queue.get()
-	answer_queue.task_done()
+    rqs = {"textarea": text}
+    r = requests.post("http://hacheck.tel.fer.hr/xml.pl", data=rqs)
+    answer_queue.put((id, (text, r.text)))
+    thread_queue.get()
+    answer_queue.task_done()
 
 
 def main():
-	rqs_limit = 2000
-	max_threads = 5
+    rqs_limit = 2000
+    max_threads = 5
 
-	answer_queue = queue.Queue()
-	thread_queue = queue.Queue()
-	thread_list = list()
+    answer_queue = queue.Queue()
+    thread_queue = queue.Queue()
+    thread_list = list()
 
-	cnt = 0
-	splitWord = "!__ENDOFTEXT__!"
-	endRQS = "!__ENDRQS__!"
+    cnt = 0
+    splitWord = "!__ENDOFTEXT__!"
+    endRQS = "!__ENDRQS__!"
 
-	for line in iter(sys.stdin.readline, ''):
-		
-		while thread_queue.qsize() > max_threads:
-			pass
+    for line in iter(sys.stdin.readline, ''):
 
-		thread_queue.put(cnt)
-		t = threading.Thread(target=send_request, args=(line, answer_queue, thread_queue, cnt))
-		t.daemon = True
-		t.start()
-		thread_list.append(t)
+        while thread_queue.qsize() > max_threads:
+            pass
 
-		cnt += 1
-		if cnt > rqs_limit:
-			break
-		time.sleep(1)
+        thread_queue.put(cnt)
+        t = threading.Thread(
+            target=send_request, args=(line, answer_queue, thread_queue, cnt))
+        t.daemon = True
+        t.start()
+        thread_list.append(t)
 
-	answer_queue.join()
-	for t in thread_list:
-		t.join()
+        cnt += 1
+        if cnt > rqs_limit:
+            break
+        time.sleep(1)
 
-	answer_list = [None]*answer_queue.qsize()
-	while not answer_queue.empty():
-		id, item = answer_queue.get() 
-		answer_list[id] = item
+    answer_queue.join()
+    for t in thread_list:
+        t.join()
 
-	for item in answer_list:
-		sys.stdout.write(item[0] + splitWord + item[1] + endRQS)
+    answer_list = [None]*answer_queue.qsize()
+    while not answer_queue.empty():
+        id, item = answer_queue.get()
+        answer_list[id] = item
+
+    for item in answer_list:
+        sys.stdout.write(item[0] + splitWord + item[1] + endRQS)
 
 if __name__ == "__main__":
-	main()
+    main()
