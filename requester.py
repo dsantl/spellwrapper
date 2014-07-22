b561e6191a73f239db7810fb87e969261156d306
budaim <budabudimir.bb@gmail.com>
2014-07-22 12:59:59 +0200

    requester.py

--- requester.py
+++ requester.py
@@ -1,66 +1,23 @@
 #!/usr/bin/env python3
 
-import sys
+import multiprocessing as mp
 import requests
-import time
-import threading
-import queue
-
-import locale
-locale.getpreferredencoding = lambda: 'UTF-8'
-
-sys.stdin = open('/dev/stdin', 'r')       # Re-open standard files in UTF-8 
-sys.stdout = open('/dev/stdout', 'w')     # mode.
-sys.stderr = open('/dev/stderr', 'w')
+import sys
 
 
-def send_request(text, answer_queue, thread_queue, id):
-	rqs = {"textarea":text}
-	r = requests.post("http://hacheck.tel.fer.hr/xml.pl", data = rqs)
-	answer_queue.put((id, (text, r.text)))
-	thread_queue.get()
-	answer_queue.task_done()
+def send_request(text):
+    data = {'textarea': text}
+    r = requests.post("http://hacheck.tel.fer.hr/xml.pl", data=data)
+    return text, r.text
 
 
-def main():
-	rqs_limit = 2000
-	max_threads = 5
+if __name__ == '__main__':
+    p = mp.Pool(5)
 
-	answer_queue = queue.Queue()
-	thread_queue = queue.Queue()
-	thread_list = list()
+    split_word = '!__ENDOFTEXT__!'
+    end_rqs = '!__ENDRQS__!'
 
-	cnt = 0
-	splitWord = "!__ENDOFTEXT__!"
-	endRQS = "!__ENDRQS__!"
+    resulsts = p.map(send_request, sys.stdin.readlines())
 
-	for line in iter(sys.stdin.readline, ''):
-		
-		while thread_queue.qsize() > max_threads:
-			pass
-
-		thread_queue.put(cnt)
-		t = threading.Thread(target=send_request, args=(line, answer_queue, thread_queue, cnt))
-		t.daemon = True
-		t.start()
-		thread_list.append(t)
-
-		cnt += 1
-		if cnt > rqs_limit:
-			break
-		time.sleep(1)
-
-	answer_queue.join()
-	for t in thread_list:
-		t.join()
-
-	answer_list = [None]*answer_queue.qsize()
-	while not answer_queue.empty():
-		id, item = answer_queue.get() 
-		answer_list[id] = item
-
-	for item in answer_list:
-		sys.stdout.write(item[0] + splitWord + item[1] + endRQS)
-
-if __name__ == "__main__":
-	main()
+    for old, new in resulsts:
+        sys.stdout.write(old + split_word + new + end_rqs)
